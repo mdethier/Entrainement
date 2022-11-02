3d950b6639256d144c7bbf1f30bdb2a2d8ba0ddf
Maxime Dethier <mdethierlaposte.net>
2022-11-02 14:45:16 +0100

    Fruits ok

--- Fruits/fruits-starter-kit/fruits.js
+++ Fruits/fruits-starter-kit/fruits.js
@@ -1,5 +1,5 @@
-let fruits = [{ name: "Orange", image: "orange.jpg" }, { name: "Kiwi", image: "kiwi.jpg" }, { name: "Banane", image: "banane.jpg" }, { name: "Peche", image: "peche.jpg" }];
-let selectedFruit= "";
+let fruits = [{ name: "Orange", image: "orange.jpg" }, { name: "Kiwi", image: "kiwi.jpg" }, { name: "Banane", image: "banane.jpg" }, { name: "Peche", image: "peche.jpg" }, { name: "Cerise", image:"cerise.jpg"}];
+let selectedFruit = "./all-fruits.jpg";
 
 
 const blockFruits = document.querySelector("#fruits");
@@ -10,29 +10,29 @@ fruits.forEach(fruit => {
   const fruitDiv = document.createElement("div");
   fruitDiv.textContent = (fruit.name);
   fruitDiv.className = "boutonFruits";
+  fruitDiv.style.height = 100/fruits.length+"vh";
   blockFruits.appendChild(fruitDiv);
 
   fruitDiv.addEventListener("mouseover", (event) => {
     blockImage.style.backgroundImage = "url(" + fruit.image + ")";
 
   });
-  fruitDiv.addEventListener("mouseout", (event) => {
 
-    blockImage.style.backgroundImage = "url(./all-fruits.jpg)";
-
-  });
   fruitDiv.addEventListener("click", (event) => {
+
     const allDivFruits = document.querySelectorAll(".boutonFruits");
     allDivFruits.forEach(divFruit => {
       divFruit.style.backgroundColor = "";
-    })
 
+    });
     fruitDiv.style.backgroundColor = "red";
-   // blockImage.style.backgroundImage = "url("+fruit.image+")";
-  });
+    selectedFruit = fruit.image;
 
+
+    // blockImage.style.backgroundImage = "url("+fruit.image+")";
+  });
   fruitDiv.addEventListener("mouseout", (event) => {
-    blockImage.style.backgroundImage = "url(" + fruit.image + ")";
+    blockImage.style.backgroundImage = "url("+selectedFruit+")";
   });
 
 });
