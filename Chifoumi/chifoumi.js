460323d52dc3c2f9900acc84ae094dccadd2465a
Maxime Dethier <mdethierlaposte.net>
2022-11-07 11:45:15 +0100

    Création Chifoumi + commentaire perso intro

---
+++ Chifoumi/chifoumi.js
@@ -0,0 +1,6 @@
+// On veut que lorsque l'image de gauche ou de droite soit cliquée, cette image soit remplacée aléatoirement par l'une des images fournies, avec une transition exprimée en millisecondes.
+
+// Ensuite, on souhaite que l'ordinateur compare les deux images selon le schéma du chifoumi, à savoir : la pierre bat les ciseaux, les ciseaux battent la feuille, la feuille bat la pierre, et dans le cas de la même image, il y a égalité.
+
+// On affiche ensuite le texte qui remplace le titre Chifoumi et le bouton Rejouer devient cliquable et permet de réinitialiser les cartes de droite et de gauche sur leur image initiale, en réaffichant le titre Chifoumi.
+
